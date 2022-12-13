b63217a047abcc11d71ec38a722636bd07adbe68
ferrarajohn <94008222+ferrarajohn@users.noreply.github.com>
2022-12-12 18:57:09 -0600

    Update Explore.jsx

--- src/components/explore/Explore.jsx
+++ src/components/explore/Explore.jsx
@@ -78,7 +78,7 @@ const Explore = (props) => {
               <div className="featured-card">
                 {royaltyReceiver ? (
                   <div className="buynow-action">
-                    {Number(royaltyPercent)}% Royalties
+                    {Number(royaltyPercent)}% royalty
                   </div>
                 ) : (
                   <></>
@@ -109,7 +109,7 @@ const Explore = (props) => {
                     </div>
                   </div>
                   <div>
-                    <div>ID: #{Number(item.itemId)}</div>
+                    <div>Item ID: #{Number(item.itemId)}</div>
                   </div>
                 </div>
                 <div>
@@ -129,7 +129,7 @@ const Explore = (props) => {
           ))}
         </div>
       ) : (
-        <div>No NFTs to display</div>
+        <div>No items to display</div>
       )}
     </div>
   );
